85cac27cac58dbbba7d3450723c2a5dcb979ff12
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2022-10-11 05:58:24 +0700

    module config

---
+++ Module.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * archive-pengolahan module definition class
+ *
+ * @author Putra Sudaryanto <putra@ommu.id>
+ * @contact (+62)856-299-4114
+ * @copyright Copyright (c) 2022 OMMU (www.ommu.id)
+ * @created date 11 October 2022, 05:55 WIB
+ * @link https://bitbucket.org/ommu/archive-pengolahan
+ *
+ */
+
+namespace ommu\archivePengolahan;
+
+use Yii;
+
+class Module extends \app\components\Module
+{
+	public $layout = 'main';
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public $controllerNamespace = 'ommu\archivePengolahan\controllers';
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function init()
+	{
+		parent::init();
+	}
+}
---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * archive-pengolahan module config
+ *
+ * @author Putra Sudaryanto <putra@ommu.id>
+ * @contact (+62)856-299-4114
+ * @copyright Copyright (c) 2022 OMMU (www.ommu.id)
+ * @created date 11 October 2022, 05:55 WIB
+ * @link https://bitbucket.org/ommu/archive-pengolahan
+ *
+ */
+
+return [
+	'id' => 'archive-pengolahan',
+	'class' => ommu\archivePengolahan\Module::className(),
+];
