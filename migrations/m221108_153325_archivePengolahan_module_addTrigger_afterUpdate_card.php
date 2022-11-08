bd6dfaed630f603ba94cd1dff1bd0165aea6c9ec
dwptr <dwptr@ommu.id>
2022-11-08 15:39:22 +0700

    fix penyerahan card: add migrate addTrigger

---
+++ migrations/m221108_153325_archivePengolahan_module_addTrigger_afterUpdate_card.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * m221108_153325_archivePengolahan_module_addTrigger_afterUpdate_card
+ * 
+ * @author Putra Sudaryanto <putra@ommu.id>
+ * @contact (+62)856-299-4114
+ * @copyright Copyright (c) 2022 OMMU (www.ommu.id)
+ * @created date 8 November 2022, 15:34 WIB
+ * @link https://bitbucket.org/ommu/archive-pengolahan
+ *
+ */
+
+use Yii;
+use yii\db\Schema;
+
+class m221108_153325_archivePengolahan_module_addTrigger_afterUpdate_card extends \yii\db\Migration
+{
+	public function up()
+	{
+        $this->execute('DROP TRIGGER IF EXISTS `archivePengolahanAfterInsertPenyerahanCard`');
+
+        // create trigger archivePengolahanAfterInsertPenyerahanCard
+        $archivePengolahanAfterInsertPenyerahanCard = <<< SQL
+CREATE
+    TRIGGER `archivePengolahanAfterInsertPenyerahanCard` AFTER INSERT ON `ommu_archive_pengolahan_penyerahan_card` 
+    FOR EACH ROW BEGIN
+	UPDATE `ommu_archive_pengolahan_users` SET `archives` = `archives` + 1 WHERE `id` = NEW.user_id;
+    END;
+SQL;
+        $this->execute($archivePengolahanAfterInsertPenyerahanCard);
+	}
+
+	public function down()
+	{
+        $this->execute('DROP TRIGGER IF EXISTS `archivePengolahanAfterInsertPenyerahanCard`');
+    }
+}
